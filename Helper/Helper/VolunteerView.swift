0921f987fb343f3dd8e676eb88303cdb80365234
Son Dang <minhson0506@gmail.com>
2022-04-01 02:38:38 +0300

    create basic project

---
+++ Helper/Helper/VolunteerView.swift
@@ -0,0 +1,25 @@
+//
+//  VolunteerView.swift
+//  Helper
+//
+//  Created by Dang Son on 1.4.2022.
+//
+
+import SwiftUI
+
+struct VolunteerView: View {
+    var body: some View {
+        Text("Home")
+        Button(action:{
+            NavigationLink(des)
+        }) {
+            Image(systemName: "forward.fill")
+        }
+    }
+}
+
+struct VolunteerView_Previews: PreviewProvider {
+    static var previews: some View {
+        VolunteerView()
+    }
+}
---
+++ Helper/HelperUITests/HelperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HelperUITestsLaunchTests.swift
+//  HelperUITests
+//
+//  Created by Dang Son on 1.4.2022.
+//
+
+import XCTest
+
+class HelperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
