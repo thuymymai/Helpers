4db3366dec5c38ad103d348115e08db9553af5b0
anniehuynh <anniehuynh253@gmail.com>
2022-04-11 19:16:17 +0300

    Task Detail UI ready

--- Helper/Helper/Views/TaskDetailView.swift
+++ Helper/Helper/Views/TaskDetailView.swift
@@ -14,20 +14,26 @@ struct TaskDetailView: View {
         ZStack{
             Color("Background")
                 .edgesIgnoringSafeArea(.all)
-            VStack{
-                ContactInfo()
-                Divider()
-                
-                DisabilityInformation()
-            }.frame(width: 330, height: 300)
-                .background(.white)
-                .cornerRadius(10)
-                .shadow(radius: 5)
-            
+            ZStack {
+                VStack{
+                    ContactInfo()
+                    Divider()
+                    DisabilityInformation()
+                }.frame(width: 330, height: 300)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+            }.position(x: 195, y: 180)
+            VStack(alignment: .leading, spacing: 8){
+                TaskTitle()
+                Address()
+                TimeNeeded()
+                TaskDescription()
+                Attachment()
+            }.padding(.horizontal)
+                .position(x: 200, y: 560)
         }
-        
     }
-    
 }
 
 struct TaskDetailView_Previews: PreviewProvider {
@@ -58,8 +64,6 @@ struct DisabilityInformation: View {
                     .font(.body)
                     .bold()
                     .foregroundColor(.black)
-                
-                
                 Text("heart disease")
                     .font(.body)
                     .foregroundColor(.black)
@@ -71,8 +75,6 @@ struct DisabilityInformation: View {
                     .font(.body)
                     .bold()
                     .foregroundColor(.black)
-                
-                
                 Text("Pollen, animal fur")
                     .font(.body)
                     .foregroundColor(.black)
@@ -97,8 +99,7 @@ struct ContactInfo: View {
             
             Text("Help seeker name")
                 .bold()
-                .padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            
+                .padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)            
             HStack(spacing:30) {
                 VStack(spacing:15) {
                     Image(systemName: "phone.fill")
@@ -117,5 +118,62 @@ struct ContactInfo: View {
                 }
             }
         }.padding(.top,0)
+    }
+}
+
+struct TaskTitle: View {
+    var body: some View {
+        Text("Title")
+            .bold()
+            .font(.title2)
+            .foregroundColor(Color("Primary"))
+        Text("Grocery Shopping")
+        
+        Divider()
+    }
+}
+
+struct Address: View {
+    var body: some View {
+        Text("Address")
+            .bold()
+            .font(.title2)
+            .foregroundColor(Color("Primary"))
+        Text("Runeberginkatu 55, 00260 Helsinki")
+        Divider()
+    }
+}
+
+struct TimeNeeded: View {
+    var body: some View {
+        Text("Time Needed")
+            .bold()
+            .font(.title2)
+            .foregroundColor(Color("Primary"))
+        Text("Runeberginkatu 55, 00260 Helsinki")
+        Divider()
+    }
+}
+
+struct TaskDescription: View {
+    var body: some View {
+        Text("Description")
+            .bold()
+            .font(.title2)
+            .foregroundColor(Color("Primary"))
+        Text("I need help picking up some items at the store while my personal assistant is on sick leave.\nLeave at front door. Entrance code B1234.")
+        Divider()
+    }
+}
+
+struct Attachment: View {
+    var body: some View {
+        Text("Attached file")
+            .bold()
+            .font(.title2)
+            .foregroundColor(Color("Primary"))
+        Label("1 attached file", systemImage: "paperclip")
+        Text("See image or file sent by help seeker")
+            .foregroundColor(Color("Primary"))
     }
 }
