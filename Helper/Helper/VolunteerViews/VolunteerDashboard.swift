955bfe600163665f287df84c5762ef9792791c79
anniehuynh <anniehuynh253@gmail.com>
2022-04-18 15:23:13 +0300

    volunteer dashboard responsive

--- Helper/Helper/VolunteerViews/VolunteerDashboard.swift
+++ Helper/Helper/VolunteerViews/VolunteerDashboard.swift
@@ -21,20 +21,14 @@ struct VolunteerDashboard: View {
                         VStack (alignment: .leading){
                             TagLineView()
                             SearchAndFilter()
-                            Text("Available Tasks")
-                                .font(.system(size: 24))
-                                .position( x:80, y: 70)
-                            Text("10 tasks waiting to be accepted")
-                                .font(.system(size: 16))
-                                .foregroundColor(Color("Primary"))
-                                .fontWeight(.medium)
-                                .position( x:120, y: 65)
-                            //ForEach(categories) {category in
-                            //                        NavigationLink(destination: AvailableTasksView(category: category)) {
-                            //                            CategoriesView(category: category)
-                            //
-                            //                        }
-                            
+                            VStack(alignment: .leading, spacing: 5){
+                                Text("Available Tasks")
+                                    .font(.system(size: 24))
+                                Text("10 tasks waiting to be accepted")
+                                    .font(.system(size: 16))
+                                    .foregroundColor(Color("Primary"))
+                                    .fontWeight(.medium)
+                            }.padding(.top, 55)
                             HStack(alignment:.center, spacing: 10) {
                                 NavigationLink(destination: AvailableTasksView()) {
                                     CategoriesView(categoryName: "Grocery", numberOfTasks: "3 Tasks", ImageName: "groceries image")
@@ -45,14 +39,12 @@ struct VolunteerDashboard: View {
                                 NavigationLink(destination: AvailableTasksView()) {
                                     CategoriesView(categoryName: "Others", numberOfTasks: "4 Tasks", ImageName: "helping image")
                                 }
-                            }.position( x:179, y: 60)
+                            }
                             
-                            Text("Ongoing Tasks")
-                                .font(.system(size: 24))
-                                .position( x:80, y: 240)
-                            OngoingTaskCard().position( x:180, y: 240)
-                                .padding(.bottom, 50)
-                        }
+                                Text("Ongoing Tasks")
+                                    .font(.system(size: 24))
+                                OngoingTaskCard()
+                        }.padding(.horizontal)
                         
                     } .padding(.horizontal)
                 }
@@ -148,28 +140,30 @@ struct CategoriesView: View {
     var numberOfTasks: String
     var ImageName: String
     var body: some View {
-        GeometryReader { geometry in
-            ZStack{
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(.white)
-                    .shadow(radius: 5)
-                    .frame(width: geometry.size.width * 1, height: geometry.size.height * 15)
-                VStack(alignment: .leading){
-                    Text(categoryName)
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .foregroundColor(.primary)
-                        .padding(.top,25)
-                    Text(numberOfTasks)
-                        .font(.subheadline)
-                        .foregroundColor(.primary)
-                    Image(ImageName)
-                        .resizable()
-                        .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 8,alignment: .center)
-                        .padding(.bottom,20)
-                }
+        
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+                .frame(width: 110, height: 140)
+                .shadow(radius: 5)
+            
+            VStack(alignment: .leading){
+                Text(categoryName)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(.primary)
+                    .padding(.top,25)
+                Text(numberOfTasks)
+                    .font(.subheadline)
+                    .foregroundColor(.primary)
+                Image(ImageName)
+                    .resizable()
+                    .frame(width: 100, height: 75,alignment: .center)
+                    .padding(.bottom,20)
             }
         }
+        
         
     }
 }
@@ -182,7 +176,7 @@ struct OngoingTaskCard: View {
                     RoundedRectangle(cornerRadius: 10)
                         .fill(.white)
                         .shadow(radius: 5)
-                        .frame(width: geometry.size.width * 1.02, height: geometry.size.height * 15, alignment: .center)
+                        .frame(width: geometry.size.width * 1.04, height: geometry.size.height * 15, alignment: .center)
                     VStack(alignment: .leading, spacing: 10){
                         HStack(spacing: 130){
                             Text("Grocery Shopping")
@@ -206,16 +200,16 @@ struct OngoingTaskCard: View {
                                 .font(.system(size: 14))
                                 .foregroundColor(.secondary)
                             NavigationLink(destination: TaskDetailView().navigationBarHidden(true)){
-//                                Button(action: {}) {
-                                    Text("View Task")
-                                        .font(.subheadline)
-                                        .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 3)
-                                        .background(Color("Primary"))
-                                        .foregroundColor(.white)
-                                        .cornerRadius(6)
+                                //                                Button(action: {}) {
+                                Text("View Task")
+                                    .font(.subheadline)
+                                    .frame(width: geometry.size.width * 0.3, height: geometry.size.height * 3)
+                                    .background(Color("Primary"))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(6)
                                 //}
                             }
-                          
+                            
                         }
                     }
                     
