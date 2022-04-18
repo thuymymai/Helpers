85d5aa3903f6c7b080715ee23d93cda9a8595a31
anniehuynh <anniehuynh253@gmail.com>
2022-04-18 11:18:31 +0300

    volunteer flow contentview change, added nav bar file

--- Helper/Helper/ContentView.swift
+++ Helper/Helper/ContentView.swift
@@ -7,17 +7,14 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
-   
     var body: some View {
         LandingPage()
     }
 }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
