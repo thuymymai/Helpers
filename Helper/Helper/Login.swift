362cae3b94130a554691eb3ff6c068ee05909652
anniehuynh <anniehuynh253@gmail.com>
2022-04-19 18:11:38 +0300

    Login logic and UI done

--- Helper/Helper/Login.swift
+++ Helper/Helper/Login.swift
@@ -3,6 +3,7 @@
 //  Helper
 //
 //  Created by Mai Thuỳ My on 9.4.2022.
+//  Contributor Annie Huynh
 //
 
 import SwiftUI
@@ -19,7 +20,7 @@ struct Login: View {
                     Image("Image-login")
                         .resizable()
                         .frame(width: 300, height: 200)
-                        .padding(.top, 50)
+                        .padding(.top,-20)
                     Spacer()
                 }
                 VStack{
@@ -29,10 +30,12 @@ struct Login: View {
                             .frame(width: 300, height: 300)
                             .shadow(radius: 5)
                         Form().environment(\.managedObjectContext, persistenceController.container.viewContext)
-                        
                     }
                     Image("Login")
-                }.padding(.top, 280)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .padding(.top,30)
+                }.padding(.top, 220)
             }
         }
     }
@@ -48,44 +51,17 @@ struct Form: View {
     @State var email: String = ""
     @State var password: String = ""
     @State var toDashboard: Bool = false
+    @State var toRegister: Bool = false
     @State var showAlert: Bool = false
     @State var isLinkActive: Bool = false
+    @State private var loginFailed = false
+    
     // set up environment
     @StateObject var userModel = UserViewModel()
     @Environment(\.managedObjectContext) var context
     
     // fetching data from core data
     @FetchRequest(entity: User.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \User.user_id, ascending: true)]) var results: FetchedResults<User>
-    
-    
-    func submit() {
-        let emails = results.map{$0.email}
-        var userType: String?
-        // get user array and check user type to navigate accordingly
-        let userInfo = results.filter{$0.email == email.lowercased()}
-        if (userInfo.count > 0)  {
-            userType = userInfo[0].type
-        } else {print("No user type found")}
-        
-        if (emails.contains(email.lowercased()) && userType == "v") {
-            
-            print("user contain true, type v")
-        } else if (emails.contains(email.lowercased()) && userType == "h"){
-            print("user contain tru,e type h")
-        } else {print("user false")}
-        //        let passwords = results.map{$0.password}
-        //        print("count \(results.count)")
-        //        print("result \(results)")
-        //        print("email \(emails)")
-        //        print("password \(passwords)")
-        //        if (emails.contains(email) && passwords.contains(password)) {
-        //            self.toDashboard = true
-        //        } else {
-        //            print("wrong information")
-        //        }
-        
-    }
-    
     
     var body: some View {
         ZStack{
@@ -109,12 +85,26 @@ struct Form: View {
                     .cornerRadius(5)
             }
             .frame(width: 250)
-                        .padding(.top, -50)
+            .padding(.top, -50)
+            
+            // conditional navigation for each flow
             NavigationLink(destination:
                             chooseDestination(), isActive: self.$isLinkActive
             ){EmptyView()}
+            
+            // navigation to sign up page
+            NavigationLink(destination: LandingPage().navigationBarHidden(true), isActive: self.$toRegister) { EmptyView() }
             Button(action: {
-                showAlert.toggle()
+                let emails = results.map{$0.email}
+                let passwords = results.map{$0.password}
+                let userExists = emails.contains(email.lowercased()) && passwords.contains(password)
+                
+                // reset to to the initial stage
+                self.loginFailed = false
+                if (!userExists){
+                    self.loginFailed.toggle()
+                }
+                self.showAlert.toggle()
             }) {
                 Text("LOG IN")
                     .fontWeight(.bold)
@@ -123,15 +113,22 @@ struct Form: View {
                     .background(Color("Primary"))
                     .foregroundColor(.white)
                     .cornerRadius(10)
-                    
+                
             }.padding(.top, 210)
-            .alert(isPresented: $showAlert, content: {
-                Alert(title: Text("Login successfully"), dismissButton: .default(Text("OK"), action: {self.isLinkActive = true}))
-            })
+                .alert(isPresented: $showAlert, content: {
+                    // alert when login is failed
+                    if self.loginFailed {
+                        return  Alert(title: Text("Failed to login"), message: Text("Check your credentials and try again or Sign Up now"), primaryButton: .default(Text("Sign Up"), action: {
+                            self.toRegister = true
+                        }), secondaryButton: .default(Text("Try again"), action: {resetForm()}))
+                    // when log in successfully
+                    } else {
+                        return  Alert(title: Text("Welcome"), message: Text("You have now logged in"), dismissButton: .default(Text("OK"), action: {self.isLinkActive = true}))
+                    }
+                })
         }
-        
     }
-    
+    // function choose destination conditionally
     @ViewBuilder
     func chooseDestination() -> some View {
         let emails = results.map{$0.email}
@@ -149,6 +146,11 @@ struct Form: View {
             }
         }
         
+    }
+    // reset form fields when email or password is incorrect
+    func resetForm(){
+        email = ""
+        password = ""
     }
     
 }
