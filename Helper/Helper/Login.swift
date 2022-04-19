340991608df2ce990ee004ab9e0c56acefe8992f
anniehuynh <anniehuynh253@gmail.com>
2022-04-19 17:29:25 +0300

    login with user check and navigate works

--- Helper/Helper/Login.swift
+++ Helper/Helper/Login.swift
@@ -12,26 +12,28 @@ struct Login: View {
     let persistenceController = PersistenceController.shared
     
     var body: some View {
-        ZStack{
-            Color("Background").edgesIgnoringSafeArea(.all)
-            VStack{
-                Image("Image-login")
-                    .resizable()
-                    .frame(width: 300, height: 200)
-                    .padding(.top, 50)
-                Spacer()
-            }
-            VStack{
-                ZStack{
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(.white)
-                        .frame(width: 300, height: 300)
-                        .shadow(radius: 5)
-                    Form().environment(\.managedObjectContext, persistenceController.container.viewContext)
-                    
+        NavigationView{
+            ZStack{
+                Color("Background").edgesIgnoringSafeArea(.all)
+                VStack{
+                    Image("Image-login")
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                        .padding(.top, 50)
+                    Spacer()
                 }
-                Image("Login")
-            }.padding(.top, 280)
+                VStack{
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                            .frame(width: 300, height: 300)
+                            .shadow(radius: 5)
+                        Form().environment(\.managedObjectContext, persistenceController.container.viewContext)
+                        
+                    }
+                    Image("Login")
+                }.padding(.top, 280)
+            }
         }
     }
 }
@@ -46,32 +48,44 @@ struct Form: View {
     @State var email: String = ""
     @State var password: String = ""
     @State var toDashboard: Bool = false
-    
+    @State var showAlert: Bool = false
+    @State var isLinkActive: Bool = false
     // set up environment
     @StateObject var userModel = UserViewModel()
     @Environment(\.managedObjectContext) var context
-
+    
     // fetching data from core data
     @FetchRequest(entity: User.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \User.user_id, ascending: true)]) var results: FetchedResults<User>
-  
+    
+    
     func submit() {
         let emails = results.map{$0.email}
-//        let passwords = results.map{$0.password}
-//        print("count \(results.count)")
-//        print("result \(results)")
-        print("email \(emails)")
-//        print("password \(passwords)")
-//        if (emails.contains(email) && passwords.contains(password)) {
-//            self.toDashboard = true
-//        } else {
-//            print("wrong information")
-//        }
-        if (emails.contains(email.lowercased())) {
-            print("user contain true")
-        } else {
-            print("user contain false")
-        }
+        var userType: String?
+        // get user array and check user type to navigate accordingly
+        let userInfo = results.filter{$0.email == email.lowercased()}
+        if (userInfo.count > 0)  {
+            userType = userInfo[0].type
+        } else {print("No user type found")}
+        
+        if (emails.contains(email.lowercased()) && userType == "v") {
+            
+            print("user contain true, type v")
+        } else if (emails.contains(email.lowercased()) && userType == "h"){
+            print("user contain tru,e type h")
+        } else {print("user false")}
+        //        let passwords = results.map{$0.password}
+        //        print("count \(results.count)")
+        //        print("result \(results)")
+        //        print("email \(emails)")
+        //        print("password \(passwords)")
+        //        if (emails.contains(email) && passwords.contains(password)) {
+        //            self.toDashboard = true
+        //        } else {
+        //            print("wrong information")
+        //        }
+        
     }
+    
     
     var body: some View {
         ZStack{
@@ -95,9 +109,13 @@ struct Form: View {
                     .cornerRadius(5)
             }
             .frame(width: 250)
-            .padding(.top, -50)
-            NavigationLink(destination: VolunteersNavBar().navigationBarBackButtonHidden(true), isActive: self.$toDashboard) { EmptyView() }
-            Button(action: {submit()}) {
+                        .padding(.top, -50)
+            NavigationLink(destination:
+                            chooseDestination(), isActive: self.$isLinkActive
+            ){EmptyView()}
+            Button(action: {
+                showAlert.toggle()
+            }) {
                 Text("LOG IN")
                     .fontWeight(.bold)
                     .font(.system(size: 14))
@@ -105,9 +123,33 @@ struct Form: View {
                     .background(Color("Primary"))
                     .foregroundColor(.white)
                     .cornerRadius(10)
-            }
-            .padding(.top, 210)
+                    
+            }.padding(.top, 210)
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text("Login successfully"), dismissButton: .default(Text("OK"), action: {self.isLinkActive = true}))
+            })
         }
         
     }
+    
+    @ViewBuilder
+    func chooseDestination() -> some View {
+        let emails = results.map{$0.email}
+        let passwords = results.map{$0.password}
+        let userInfo = results.filter{$0.email == email.lowercased()}
+        let checkUserExists = emails.contains(email.lowercased()) && passwords.contains(password)
+        
+        if (userInfo.count > 0)  {
+            if  (checkUserExists && userInfo[0].type == "v") {
+                VolunteersNavBar().navigationBarHidden(true)
+            } else if (checkUserExists && userInfo[0].type == "h") {
+                FrontScreen().navigationBarHidden(true)
+            } else {
+                EmptyView()
+            }
+        }
+        
+    }
+    
 }
+
