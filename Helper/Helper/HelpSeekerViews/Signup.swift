2618f428f2d29a2aa61ad6e1893e2e4763f55a13
Amy Mai <My.Mai@metropolia.fi>
2022-04-20 11:33:59 +0700

    signup without save data ready

--- Helper/Helper/HelpSeekerViews/Signup.swift
+++ Helper/Helper/HelpSeekerViews/Signup.swift
@@ -53,6 +53,18 @@ struct FormView: View {
     @State var password: String = ""
     @State var confirm: String = ""
     @State var isLinkActive = false
+    @State private var signupFailed = false
+    @State private var showAlert: Bool = false
+    @State private var passwordFailed = false
+    @State private var emptyField = false
+    
+    // set up environment
+    @StateObject var userModel = UserViewModel()
+    @Environment(\.managedObjectContext) var context
+    
+    // fetching data from core data
+    @FetchRequest(entity: User.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \User.user_id, ascending: true)]) var results: FetchedResults<User>
+    
     var body: some View {
         ZStack{
             VStack {
@@ -107,7 +119,21 @@ struct FormView: View {
             NavigationLink(
                 destination: MedicalInfo().navigationBarHidden(true), isActive: $isLinkActive) {
                     Button(action: {
-                        self.isLinkActive = true
+                        let emails = results.map{$0.email}
+                        let userExists = emails.contains(email.lowercased())
+                        self.signupFailed = false
+                        if (userExists){
+                            self.signupFailed.toggle()
+                        }
+                        self.emptyField = false
+                        if(fullname == "" || email == "" || phone == ""  || password == "" || confirm == "") {
+                            self.emptyField.toggle()
+                        }
+                        self.passwordFailed = false
+                        if(confirm != password){
+                            self.passwordFailed.toggle()
+                        }
+                        self.showAlert.toggle()
                     }) {
                         Text("NEXT")
                             .font(.system(size: 16))
@@ -117,10 +143,23 @@ struct FormView: View {
                             .resizable()
                             .frame(width: 25, height: 25)
                             .foregroundColor(Color("Primary"))
-
+                        
                     }
                     .padding(.top, 550)
                     .frame(alignment: .trailing)
+                    .alert(isPresented: $showAlert, content: {
+                        // alert when email taken
+                        if self.signupFailed {
+                            return Alert(title: Text("Sign up failed"), message: Text("Email already taken!"), dismissButton: .default(Text("Try again")))
+                            //alert when password not match
+                        } else if self.emptyField {
+                            return Alert(title: Text("Sign up failed"), message: Text("All fields are required!"), dismissButton: .default(Text("Try again")))
+                        } else if self.passwordFailed {
+                            return Alert(title: Text("Sign up failed"), message: Text("Passwords do not match!"), dismissButton: .default(Text("Try again")))
+                        } else {
+                            return  Alert(title: Text("Welcome"), message: Text("You have registered successfully!"), dismissButton: .default(Text("OK"), action: {self.isLinkActive = true}))
+                        }
+                    })
                 }
         }
         
