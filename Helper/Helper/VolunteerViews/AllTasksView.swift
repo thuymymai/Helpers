3bd336cfb4e139c1a990f1c1e0997854b2263f73
anniehuynh <anniehuynh253@gmail.com>
2022-04-26 09:39:51 +0300

    help seeker UI fixed

--- Helper/Helper/VolunteerViews/AllTasksView.swift
+++ Helper/Helper/VolunteerViews/AllTasksView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AllTasksView: View {
     @FetchRequest(entity: User.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \User.userId, ascending: true)]) var results: FetchedResults<User>
     @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Task.title, ascending: true)]) var taskResults: FetchedResults<Task>
-    
+
     func getHelpseeker(task: Task) -> User {
         for user in results {
             if (user.userId == task.helpseeker) {
